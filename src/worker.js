8e83862098db3242bd619524f6b59cb3eefc0690
Matthias Liffers <m@tthi.as>
2025-10-03 14:27:35 +0800

    Clean up

--- src/worker.js
+++ src/worker.js
@@ -1,11 +1,5 @@
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
+ * Proof of concept for RAiD resolver piggybacking on DOI resolver using a Cloudflare worker
  */
 
 const DOI_HOST = "doi.org"
@@ -18,12 +12,7 @@ async function handleRequest(request) {
   const pathname = url.pathname
   const search = url.search
   const pathWithParams = pathname + search
-  // console.log("Path name:", pathname)
-  // console.log("Regex:", raidRegex)
-  // console.log("Regex matches:", pathname.match(raidRegex))
-  // console.log("Starts with:", pathname.startsWith(raidString))
   if (pathname.match(raidRegex)) {
-  // if (pathname.startsWith(raidString)) {
       return retrieveDOI(pathWithParams)
   } else {
       return retrieveRAID(pathWithParams)
@@ -32,9 +21,6 @@ async function handleRequest(request) {
 
 async function retrieveDOI(pathname) {
   const response = await fetch(`https://${DOI_HOST}${pathname}`)
-  // console.log(response.url)
-  // console.log(response.redirected)
-  // console.log(response.status)
   if (response.redirected && !response.url.startsWith("https://doi.org")) {
     return Response.redirect(response.url, 301)
   } else if ( response.status == 404) {
