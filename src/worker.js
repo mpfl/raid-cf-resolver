b71ba5fd2f4811975e48536fb15fc5cf8804f526
Matthias Liffers <m@tthi.as>
2025-10-07 15:46:48 +0800

    Maybe this way?

--- src/worker.js
+++ src/worker.js
@@ -24,7 +24,7 @@ async function retrieveDOI(pathname) {
   if (response.redirected && !response.url.startsWith("https://doi.org")) {
     return Response.redirect(response.url, 301)
   } else if ( response.status == 404) {
-    let errorResponse = this.env.ASSETS.fetch("404.html")
+    let errorResponse = this.env.ASSETS.fetch(request)
     return new Response(errorResponse.body, {status: 404})
   }
   return response
