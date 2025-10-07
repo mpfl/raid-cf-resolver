b802dc22b86545297199811df14d8d1b52630f16
Matthias Liffers <m@tthi.as>
2025-10-07 16:11:50 +0800

    Use some AI help

--- src/worker.js
+++ src/worker.js
@@ -24,7 +24,8 @@ async function retrieveDOI(pathname) {
   if (response.redirected && !response.url.startsWith("https://doi.org")) {
     return Response.redirect(response.url, 301)
   } else if ( response.status == 404) {
-    return env.ASSETS.fetch(request)
+    const url = new URL('404.html', request.url);
+    return env.ASSETS.fetch(url)
   }
   return response
 }
