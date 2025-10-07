3426987a17157c2747ddf35f8710125990534198
Matthias Liffers <m@tthi.as>
2025-10-07 16:12:15 +0800

    No semicolons here

--- src/worker.js
+++ src/worker.js
@@ -24,7 +24,7 @@ async function retrieveDOI(pathname) {
   if (response.redirected && !response.url.startsWith("https://doi.org")) {
     return Response.redirect(response.url, 301)
   } else if ( response.status == 404) {
-    const url = new URL('404.html', request.url);
+    const url = new URL('404.html', request.url)
     return env.ASSETS.fetch(url)
   }
   return response
